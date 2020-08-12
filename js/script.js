9c196db31f0e4bfc3aaaac9e046b4af34f02e688
Ivan Konchinin <vanish7@mail.ru>
2020-08-12 18:37:43 +0300

    feat: change solution of the task in js/script.js

--- js/script.js
+++ js/script.js
@@ -9,15 +9,24 @@ for (let i = 0; i < arr.length; i++){
 }
 
 // 2)
+
 let n = 100;
 
-nextStep:
-for (let i = 2; i <= n; i++) {
-
-  for (let j = 2; j < i; j++) {
-    if (i % j === 0){
-      continue nextStep;
+for(let i = 2; i <= n; i++){
+  let flag = true;
+  for(let j = 2; j <= i; j++) {
+    if(i % j === 0 && i!== j){
+       flag = false;
+      break;
+    }else{
+       flag = true;
     }
+    }
+  if (flag === true) {
+    console.log(i + ' - Делители этого числа: ' + 1 + ' и ' + i);
   }
-  console.log(i + ' - Делители этого числа: ' + 1 + ' и ' + i);
 }
+   
+
+
+
