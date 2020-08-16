e26b4cd49a964a377fc77c5b77df53ccb6346a82
Ivan Konchinin <vanish7@mail.ru>
2020-08-16 15:33:47 +0300

    fix: change "currentDay" in js/script.js

--- js/script.js
+++ js/script.js
@@ -3,26 +3,27 @@
 function weekDays(){
 
   let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-  let currentDay = new Date();
-  currentDay = currentDay.getDay() - 1;
+  let currentDay = (new Date().getDay() === 0) ? 6 : new Date().getUTCDay();
+  let out = document.querySelector('.out');
 
   function weekDaysFunc(){
-
-    week.forEach(function (item) {
-
-      if(item === 'Суббота' || item === 'Воскресенье'){
-        document.querySelector('.out').innerHTML += '<i>' + item + '</i>' + "<br>"; 
-      } else if (item === week[currentDay]){
-        document.querySelector('.out').innerHTML += '<b>' + item + '</b>' + "<br>"; 
-      } else {
-        document.querySelector('.out').innerHTML += item + "<br>"; 
-      }
-    });
-
+    for (var i = 0; i < week.length; ++i) {
+        if(i === currentDay){
+          if (week[i] === 'Суббота' || week[i] === 'Воскресенье') {
+            out.innerHTML += '<b><i>' + week[i] + '</i></b><br>';
+          }
+        }
+        else if (week[i] === 'Суббота' || week[i] === 'Воскресенье'){
+          out.innerHTML += '<i>' + week[i] + '</i><br>';
+        }
+        else {
+          out.innerHTML += week[i] + '<br>';
+        }
+    }
   }  
   return weekDaysFunc();
 }
 
 weekDays();
 
-
+console.log(new Date().getUTCDay());
