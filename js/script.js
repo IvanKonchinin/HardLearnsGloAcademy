82f40f6edb0fb2fc57540f558c3b018a50579f1b
Ivan Konchinin <vanish7@mail.ru>
2020-08-18 20:38:22 +0300

    fix:add else if in js/script.js

--- js/script.js
+++ js/script.js
@@ -5,12 +5,19 @@ function weekDays(){
   let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
   let currentDay = (new Date().getDay() === 0) ? 6 : new Date().getUTCDay();
   let out = document.querySelector('.out');
+  console.log(currentDay);
 
   function weekDaysFunc(){
     for (var i = 0; i < week.length; ++i) {
         if(i === currentDay){
           if (week[i] === 'Суббота' || week[i] === 'Воскресенье') {
             out.innerHTML += '<b><i>' + week[i] + '</i></b><br>';
+          }
+          else if (i === currentDay) {
+            out.innerHTML += '<b>' + week[i] + '</b>' + '<br>';
+          }
+          else{
+            out.innerHTML += week[i] + '<br>';
           }
         }
         else if (week[i] === 'Суббота' || week[i] === 'Воскресенье'){
@@ -25,3 +32,6 @@ function weekDays(){
 }
 
 weekDays();
+// let currentDay = new Date();
+// console.log(currentDay.toLocaleString('ru-RU', { hour12: false }));
+// console.dir(currentDay.getDay());
